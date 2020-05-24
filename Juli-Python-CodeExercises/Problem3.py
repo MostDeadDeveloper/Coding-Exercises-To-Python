865ffd4a6f132cfe21794bd6ce2f909fe57bb586
MostDeadDeveloper <vinascarljohn.125@gmail.com>
2020-05-24 18:55:50 +0800

    Problem 3

---
+++ Juli-Python-CodeExercises/Problem3.py
@@ -0,0 +1,60 @@
+"""
+Problem #3 - Just A Box 
+
+
+Problem: There really is no problem, just print me a box, cause why not. 
+The Box is specified by N by M Size.
+With N being the length and M being the Width of The Box.
+Print a box using the "X" Character to display the box.
+
+Constraint:
+2 <= N,M >= 100
+
+Minimum Time Complexity : O(n)
+
+Inputs:
+The First Input would be the N length of the box.
+The Second Input would be the M Width of the Box.
+
+Output:
+Output the Box using the "X" character, while following the length and width specified.
+ 
+Sample Input 1 :
+3
+3
+
+Sample Output 1 :
+XXX
+XXX
+XXX
+
+Sample Input 2 :
+10
+10
+
+Sample Output 2 :
+XXXXXXXXXX
+XXXXXXXXXX
+XXXXXXXXXX
+XXXXXXXXXX
+XXXXXXXXXX
+XXXXXXXXXX
+XXXXXXXXXX
+XXXXXXXXXX
+XXXXXXXXXX
+XXXXXXXXXX
+
+Sample Input 3:
+4
+5
+
+Sample Output 3:
+XXXXX
+XXXXX
+XXXXX
+XXXXX
+
+
+Really Good Hint: String Multiplication will help you fullfilling the minimum time complexity.
+
+"""
